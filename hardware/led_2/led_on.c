4973de98ad80034c3216dae2aee4910d1c959b43
xiaoerbuyu456 <49636241+xiaoerbuyu456@users.noreply.github.com>
2020-07-24 16:27:32 +0800

    Update led_on.c

--- hardware/led_2/led_on.c
+++ hardware/led_2/led_on.c
@@ -1,11 +1,13 @@
 int main()
 {
 	/*config GPF*/
-	int pGPFCON = (*((volatile unsigned int *)0x56000050));//int * pGPFCON =  (unsigned int *)0x56000050;
+	#define pGPFCON  (*((volatile unsigned int *)0x56000050));//int * pGPFCON = 0x56000050;and *pGPFCON = 0x100;
 	pGPFCON = 0X100;
 	/*write data to control led on/off */
-	int pGPFDAT = (*((volatile unsigned int *)0x56000056));
+	#define pGPFDAT  (*((volatile unsigned int *)0x56000056));
 	pGPFDAT = 0X10;
 	pGPFDAT =0;
+	
+	return 0;
 }
 
