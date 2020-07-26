5b8c3ddf4ffb82bc35031642c3bb1b0df4e0d698
xiaoerbuyu456 <1003095910@qq.com>
2020-07-26 22:33:12 +0800

    modify led file

--- hardware/led_2/led_on.c
+++ hardware/led_2/led_on.c
@@ -1,13 +1,13 @@
 int main()
 {
-	/*config GPF*/
-	#define pGPFCON  (*((volatile unsigned int *)0x56000050));//int * pGPFCON = 0x56000050;and *pGPFCON = 0x100;
-	pGPFCON = 0X100;
-	/*write data to control led on/off */
-	#define pGPFDAT  (*((volatile unsigned int *)0x56000056));
-	pGPFDAT = 0X10;
-	pGPFDAT =0;
+    /*config GPF*/
+    int* pGPFCON =  (unsigned int *)0x56000050;//int * pGPFCON = 0x56000050;and *pGPFCON = 0x100;
+    *pGPFCON = 0X100;
+    /*write data to control led on/off */
+    int * pGPFDAT=  (unsigned int *)0x56000056;
+    //*pGPFDAT = 0X10;
+    *pGPFDAT = 0;
 	
-	return 0;
+    return 0;
 }
 
