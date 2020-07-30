d02e1322bd908d8ac5ba2a22520cfc423d95087d
xiaoerbuyu456 <1003095910@qq.com>
2020-07-30 23:02:35 +0800

    change code struct

---
+++ include/sc2440_soc.h
@@ -0,0 +1,11 @@
+/*
+*This file is about register define.
+*
+*
+*/
+
+#define   GPFCON        (*(volatile unsigned long *)0x56000050)  //config GPF
+#define   GPFDAT        (*(volatile unsigned long *)0x56000056)  //write data to control GPF
+
+
+
